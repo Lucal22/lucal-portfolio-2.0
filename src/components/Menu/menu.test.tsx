4842b828340bf88bd8b0ed5993b035bc427d5487
Luis Carlos <lucaldev22@gmail.com>
2022-10-24 19:51:20 -0300

    More tests

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,5 +1,8 @@
 import { fireEvent, screen } from '@testing-library/react';
+import 'jest-styled-components';
 import React from 'react';
+import { css } from 'styled-components';
+import { theme } from '../../styles/theme';
 import { renderTheme } from '../../utils/renderTheme';
 import Menu from './index';
 
@@ -9,11 +12,24 @@ describe('<Menu />', () => {
     expect(menu).toMatchSnapshot;
   });
 
-  it('should render menu mobile and button to open and close menu', () => {
+  it('should render Button', () => {
     renderTheme(<Menu />);
     const button = screen.getByLabelText('Abre/Fecha Menu');
-    //const menuContent = button.nextSibling;
-    expect(button).toBeInTheDocument();
+    expect(button).toHaveStyleRule('display', 'flex');
+    expect(button).toHaveStyleRule('display', 'none', {
+      media: `(min-width:${theme.screen.size.medium})`,
+    });
+  });
+
+  it('should render Menu', () => {
+    renderTheme(<Menu />);
+    const button = screen.getByLabelText('Abre/Fecha Menu');
+    const menuContent = button.nextSibling;
+    console.log(menuContent);
     fireEvent.click(button);
+    expect(menuContent).toHaveStyleRule('display', 'none');
+    expect(menuContent).toHaveStyleRule('display', 'none', {
+      media: `(min-width:${theme.screen.size.medium})`,
+    });
   });
 });
